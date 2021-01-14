91acfa611971ac6e9fe3fd1140c319c8abd27b5f
TheBoringDude <iamcoderx@gmail.com>
2021-01-14 23:10:57 +0800

    Handle 404 error redirects

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -1,29 +1,36 @@
 import { useEffect } from 'react'
+import Error from 'next/error'
 
 const { DOMAIN_URL } = process.env
 
 // handles redirections for shortlinks
 export default function Redirect({ data }) {
-  useEffect(() => {
-    if (data) {
-      window.location.href = data.redirect
-    }
-  }, [data])
-
+  // data == null
   if (!data) {
-    // return the error page as default
-    return <div>404 Not Found</div>
+    // return 404 error
+    // this will be edited in the future
+    // todo: add a simple error template
+    return <Error statusCode={404} />
   }
+
+  // if there is data
+  // redirect to it
+  useEffect(() => {
+    window.location.href = data.redirect
+  }, [data])
 
   return null
 }
 
 export async function getServerSideProps(context) {
   // query data
+  // DOMAIN_URL -> should end with `/` (trailing slash)
   const res = await fetch(`${DOMAIN_URL}api/get/${context.params.redirect}`)
 
   var data = {}
 
+  // other statuscodes returns null
+  // to the data variable
   if (res.status === 200) {
     data = await res.json()
   } else {
