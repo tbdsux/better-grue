6d70d3796f952b48550f89953f8459153a60ca30
TheBoringDude <iamcoderx@gmail.com>
2021-01-14 22:48:19 +0800

    Add footer

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import Layout, { WebsiteName } from '../components/Layout'
 import Navbar from '../components/Navbar'
 import Showcase from '../components/index/Showcase'
@@ -21,6 +22,25 @@ export default function Home() {
 
       {/* api documentation */}
       <APIDocumentation />
+
+      {/* simple footer */}
+      <footer className="w-4/5 mx-auto text-center py-8 text-gray-500 text-xl">
+        <a href="https://www.buymeacoffee.com/theboringdude" target="_blank">
+          <div className="h-16 w-56 mx-auto relative">
+            <Image
+              alt="Buy Me A Coffee"
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+              layout="fill"
+            />
+          </div>
+        </a>
+        <div className="mt-4">
+          &copy; {new Date().getFullYear()} |{' '}
+          <a href="#" className="underline hover:text-gray-600 font-black">
+            @TheBoringDude
+          </a>
+        </div>
+      </footer>
     </Layout>
   )
 }
