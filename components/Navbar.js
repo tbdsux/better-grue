660e945166eddf952c44259c6d56828847e1bf8e
TheBoringDude <iamcoderx@gmail.com>
2021-04-16 17:40:23 +0800

    fix:: minor changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,34 @@
 export default function Navbar() {
-  return (
-    <div className="w-full py-4">
-      <nav className="w-11/12 md:w-4/5 mx-auto flex items-center justify-between">
-        <div className="flex items-center">
-          <img
-            src="https://img.icons8.com/flat_round/64/000000/crane-hook.png"
-            alt=""
-            className="w-12 h-12"
-          />
-          <h1 className="text-2xl font-black ml-2 tracking-wide text-green-500">
-            Grue.cf
-          </h1>
-        </div>
-        <ul className="text-xl text-gray-400 flex items-center">
-          <li className="pr-2 md:pr-8 tracking-wide font-black">
-            <a href="#api" className="hover:text-green-400">
-              API
-            </a>
-          </li>
-          <li className="pl-2 md:pl-8 tracking-wide font-black">
-            <a
-              target="_blank"
-              href="https://github.com/TheBoringDude/better-grue"
-              className="hover:text-green-400"
-            >
-              Github
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+	return (
+		<div className="w-full py-4">
+			<nav className="w-11/12 md:w-4/5 mx-auto flex items-center justify-between">
+				<div className="flex items-center">
+					<img
+						src="https://img.icons8.com/flat_round/64/000000/crane-hook.png"
+						alt=""
+						className="w-12 h-12"
+					/>
+					<h1 className="text-2xl font-black ml-2 tracking-wide text-green-500">
+						grue.cf
+					</h1>
+				</div>
+				<ul className="text-xl text-gray-400 flex items-center">
+					<li className="pr-2 md:pr-8 tracking-wide font-black">
+						<a href="#api" className="hover:text-green-400">
+							API
+						</a>
+					</li>
+					<li className="pl-2 md:pl-8 tracking-wide font-black">
+						<a
+							target="_blank"
+							href="https://github.com/TheBoringDude/better-grue"
+							className="hover:text-green-400"
+						>
+							Github
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
 }
--- components/index/Showcase.js
+++ components/index/Showcase.js
@@ -1,29 +1,32 @@
-import Image from 'next/image'
-import ShrinkForm from './ShrinkForm'
+import Image from "next/image";
+import ShrinkForm from "./ShrinkForm";
 
 // Index SHOWCASE-FORM
 export default function Showcase() {
-  return (
-    <div className="py-20 w-4/5 mx-auto">
-      <div className="flex flex-col md:flex-row items-center justify-between">
-        <div className="w-full md:w-3/4">
-          <h1 className="text-5xl lg:text-6xl font-black tracking-wide text-gray-600">
-            <span className="text-green-500">Simple & Minimalist</span> <br />{' '}
-            URL Shrinker
-          </h1>
-          <p className="pt-4 text-2xl md:text-3xl tracking-wide font-light text-gray-500">
-            Shorten unlimited long urls for free
-          </p>
-          <ShrinkForm />
-        </div>
-        <div className="relative w-full sm:w-1/2 h-96">
-          <Image
-            src="/showcase.svg"
-            layout="fill"
-            alt="Better Grue Banner Image"
-          />
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="py-20 w-4/5 2xl:w-1/2 mx-auto">
+			<div className="flex flex-col md:flex-row items-center justify-between">
+				<div className="w-full md:w-3/4">
+					<h1 className="text-5xl lg:text-6xl font-black tracking-wide text-gray-600">
+						<span className="text-green-500">
+							Simple & Minimalist
+						</span>{" "}
+						<br /> URL Shrinker
+					</h1>
+					<p className="pt-4 text-2xl md:text-3xl tracking-wide font-light text-gray-500">
+						Shorten unlimited long urls for free
+					</p>
+					<ShrinkForm />
+				</div>
+				<div className="">
+					<Image
+						src="/showcase.svg"
+						height={500}
+						width={500}
+						alt="Better Grue Banner Image"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
