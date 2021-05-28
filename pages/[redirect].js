3f795e41719a828c1fb5c6669f9f719f19dcb1b2
TheBoringDude <iamcoderx@gmail.com>
2021-05-28 18:26:26 +0800

    use SSR in `redirect` page

--- pages/[redirect].js
+++ pages/[redirect].js
@@ -1,32 +1,56 @@
-import { useEffect, useState } from "react";
-
 import Error from "next/error";
-import { useRouter } from "next/router";
+import Router from "next/router";
+import urljoin from "url-join";
 
 // handles redirections for shortlinks
-export default function RedirectFromUrl() {
-	const [error, setError] = useState(false);
-	const router = useRouter();
-	const { redirect } = router.query;
+export default function RedirectFromUrl({ q }) {
+	// show error if none
+	if (q.error) <Error statusCode={404} />;
 
-	useEffect(() => {
-		if (redirect) {
-			fetch(`/api/get/${redirect}`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ url: redirect }),
-			})
-				.then((res) => res.json())
-				.then((data) => {
-					router.push(data.redirect);
-				})
-				.catch(() => {
-					setError(true);
-				});
-		}
-	}, [redirect]);
-
-	return <>{error ? <Error statusCode={404} /> : null}</>;
+	// make sure to redirect if not error
+	Router.push(q.data.redirect);
 }
+
+export const getServerSideProps = async (context) => {
+	const { redirect } = context.params;
+
+	const url = urljoin(process.env.DOMAIN_URL, `api/get/${redirect}`);
+
+	const q = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ url: redirect }),
+	})
+		.then((r) => r.json())
+		.then((d) => {
+			return {
+				error: false,
+				data: d,
+			};
+		})
+		.catch(() => {
+			return {
+				error: true,
+				data: null,
+			};
+		});
+
+	// redirect if not error
+	if (!q.error) {
+		return {
+			redirect: {
+				permanent: true,
+				destination: q.data.redirect,
+			},
+		};
+	}
+
+	// return props
+	return {
+		props: {
+			q,
+		},
+	};
+};
